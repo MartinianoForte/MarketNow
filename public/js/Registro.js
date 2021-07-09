03a9683b074beaac8aacf6786348c8d4524dc24b
Polvulja <67106904+Polvulja@users.noreply.github.com>
2021-07-08 22:11:52 -0300

    Update Registro.js

--- public/js/Registro.js
+++ public/js/Registro.js
@@ -1,53 +1,24 @@
-const propiedades={
-	nombre :/^[a-zA-ZÀ-ÿ\s]{1,40}$/,;
-    electrico:/^[a-zA-Z0-9_.+-].{1,20}+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.].{1,20}$/,;
-	contra:/^[a-zA-Z0-9_.+-].{4,20}$/,;
-	contra2/^[a-zA-Z0-9_.+-].{4,20}$/,;
-	telefono/^[0-9]{1,10}$/,;
-	dni/^[0-9]{1,9}$/,;
-	direccion/^[a-zA-Z0-9_.+-].{1,30}+[0-9]{1,10}$/,;
-	nom_local;
-}
-const inputs = document.querySelectorAll('#formulario input');
+/*---------------Barra de Menu de roles-------------------*/
+function Barra_Roles() {
+  if (document.getElementById('comerciante').selected ==true ) {
+    console.log("chupame el pingo")
+    document.getElementById('activacion').style.display="block";
+    document.getElementById('esconder_logo').style.display="block";
+  }
+  if (document.getElementById('proveedor').selected ==true) {
+  document.getElementById('activacion').style.display="block";
+  document.getElementById('esconder_logo').style.display="none";
+  }
 
-const campos = {
-	nombre: false,
-	password: false,
-	electrico: false,
-	telefono: false,
-	dni:false,
-	nom_local:false,
+  if (document.getElementById('usuario').selected ==true) {
+    console.log("chupame el pingo")
+    document.getElementById('activacion').style.display="none";
+  }
 
-}
-const validar_formulario=(e)={
-switch (e.target.name) {
-		case "contra":
-			validarCampo(propiedades.contra, e.target, 'contra');
-			validarcontra2();
-		break;
-		case "contra2":
-			validarcontra2();
-		break;
-	}
-}
-const validarcontra2=()=>{
-const inputcontra1 = document.getElementById('contra');
-	const inputcontra2 = document.getElementById('contra2');
+  if (document.getElementById('nada').selected ==true) {
+    console.log("chupame el pingo")
+    document.getElementById('activacion').style.display="none";
+  }
+} 
 
-	if(inputcontra.value !== inputcontra2.value){
-		document.getElementById(`grupo__contra2`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__contra2`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__contra2 i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__contra2 i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__contra2 .formulario__input-error`).classList.add('formulario__input-error-activo');
-		campos['contra'] = false;
-	} else {
-		document.getElementById(`grupo__contra2`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__contra2`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__contra2 i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__contra2 i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__contra2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
-		campos['contra'] = true;
-	}
-
-}
+/*---------------Validacion de Datos-------------------*/
