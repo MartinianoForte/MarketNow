5ac7074f187059c5536919a8b480a10527175e83
Vuljanic <Polvulja@gmail.com>
2021-07-12 02:18:13 -0300

    actualice registro.js
    Lo hice adelgazar con 152 ldc

--- public/js/Registro.js
+++ public/js/Registro.js
@@ -16,173 +16,99 @@ function Barra_Roles() {
 function validar_nombre(nombre){
   let cb=document.getElementById('colorborde0');
   if (nombre.value == "") {
-    document.getElementById('texto-error').style.display="block";
-    document.getElementById('incorrecto').style.display="block";
-    document.getElementById('correcto0').style.display="none";
-    cb.style.borderColor= 'red';
-    nombre.focus();
-    validacion[0] = 0;
+    validacion_incorrecta(0,cb);
     return false;
   }
   else if (nombre.value!="") 
     {
-    document.getElementById('correcto0').style.display="block";
-    document.getElementById('texto-error').style.display="none";
-    document.getElementById('incorrecto').style.display="none";
-    cb.style.borderColor= 'green';
-    if (validacion[0] == 0) validacion[0] = 1;
+    validacion_correcta(0,cb);
     return false;
   }
 }
 function validar_mail(mail){
-   let cb2=document.getElementById('colorborde1');
+   let cb=document.getElementById('colorborde1');
    var mailRevi = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
     if (mail.value == "") {
-    document.getElementById('texto-error1').style.display="block";
-    document.getElementById('incorrecto1').style.display="block";
-    document.getElementById('correcto1').style.display="none";
-    cb2.style.borderColor= 'red';
-    validacion[1] = 0;
+    validacion_incorrecta(1,cb);
     }
   else if (mailRevi.test(mail.value)){
-    document.getElementById('texto-error1').style.display="none";
-    document.getElementById('incorrecto1').style.display="none";
-    document.getElementById('correcto1').style.display="block";
-    cb2.style.borderColor= 'green';
-    if (validacion[1] == 0) validacion[1] = 1;
+    validacion_correcta(1,cb);
   }
   else if (mail.value!=/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i || mail.value==/^[-\w.%+]{1,64}/){
-    document.getElementById('texto-error1').style.display="block";
-    document.getElementById('incorrecto1').style.display="block";
-    document.getElementById('correcto1').style.display="none";
-    cb2.style.borderColor= 'red';
-    if (validacion[1] == 0) validacion[1] = 1;
+    validacion_incorrecta(1,cb);
   }
   return false;
 }
 function validar_contra(contra){
-  var cb3=document.getElementById('colorborde2');
+  var cb=document.getElementById('colorborde2');
   var revi_contra=/^[a-z0-9_-]{4,20}$/;
   if (contra.value == "" ||contra.value == "0" ) {
-    document.getElementById('texto-error2').style.display="block";
-    document.getElementById('incorrecto2').style.display="block";
-    document.getElementById('correcto2').style.display="none";
-    cb3.style.borderColor= 'red';
-    validacion[2] = 0;
+    validacion_incorrecta(2,cb);
   }
   if (revi_contra.test(contra.value)){
-    document.getElementById('texto-error2').style.display="none";
-    document.getElementById('incorrecto2').style.display="none";
-    document.getElementById('correcto2').style.display="block";
-    cb3.style.borderColor= 'green';
-    if (validacion[2] == 0) validacion[2] = 1;
+    validacion_correcta(2,cb);
   }
   else{
-    document.getElementById('texto-error2').style.display="block";
-    document.getElementById('incorrecto2').style.display="block";
-    document.getElementById('correcto2').style.display="none";
-    cb3.style.borderColor= 'red';
-    validacion[2] = 0;
+    validacion_incorrecta(2,cb);
   }
   return false;
 }
 function validar_contra2(contra2){
-  var cb4=document.getElementById('colorborde3');
+  var cb=document.getElementById('colorborde3');
   var contra = document.getElementById('contra');
     if ((contra2.value == "") || (contra2.value != contra.value)) {
-    document.getElementById('texto-error3').style.display="block";
-    document.getElementById('incorrecto3').style.display="block";
-    document.getElementById('correcto3').style.display="none";
-    cb4.style.borderColor= 'red';
-    validacion[3] = 0;
+    validacion_incorrecta(3,cb);
     }
     else if (contra2.value == contra.value){
-    document.getElementById('texto-error3').style.display="none";
-    document.getElementById('incorrecto3').style.display="none";
-    document.getElementById('correcto3').style.display="block";
-    cb4.style.borderColor= 'green';
-    if (validacion[3] == 0) validacion[3] = 1;
+    validacion_correcta(3,cb);
     }
     return false;
 }
 function validar_telefono(telefono){
-   var cb5=document.getElementById('colorborde4');
+   var cb=document.getElementById('colorborde4');
    var telefono_revi = /^\d{8,11}$/;
    const telefonoValidado = telefono_revi.test(telefono.value);
     if ((telefono.value == "") || !telefonoValidado) {
-    document.getElementById('texto-error4').style.display="block";
-    document.getElementById('incorrecto4').style.display="block";
-    document.getElementById('correcto4').style.display="none";
-    cb5.style.borderColor= 'red';
+    validacion_incorrecta(4,cb);
     }
    else{
-    document.getElementById('texto-error4').style.display="none";
-    document.getElementById('incorrecto4').style.display="none";
-    document.getElementById('correcto4').style.display="block";
-    cb5.style.borderColor= 'green';
-    if (validacion[4] == 0) validacion[4] = 1;
+    validacion_correcta(4,cb);
   }
   return false;
 }
 function validar_dni(dni){
-  var cb6=document.getElementById('colorborde5');
+  var cb=document.getElementById('colorborde5');
   var revi_dni = /^\d{7,8}$/;
   const dniValidado = revi_dni.test(dni.value)
     if ((dni.value == "") || !dniValidado) {
-    document.getElementById('texto-error5').style.display="block";
-    document.getElementById('incorrecto5').style.display="block";
-    document.getElementById('correcto5').style.display="none";
-    validacion[3] = 0;
-    cb6.style.borderColor= 'red';
+    validacion_incorrecta(5,cb);
     }
   else {
-    document.getElementById('texto-error5').style.display="none";
-    document.getElementById('incorrecto5').style.display="none";
-    document.getElementById('correcto5').style.display="block";
-    cb6.style.borderColor= 'green';
-    if (validacion[5] == 0) validacion[5] = 1;
+    validacion_correcta(5,cb);
   }
   return false;
 }
-
 function validar_direccion(direccion){
-  var cb7=document.getElementById('colorborde6');
+  var cb=document.getElementById('colorborde6');
   var revi_direc=/^[a-z0-9_-]{4,50}$/;
   const direccionValidado = revi_direc.test(direccion.value);
     if ((direccion.value == "") || !direccionValidado) {
-    document.getElementById('texto-error6').style.display="block";
-    document.getElementById('incorrecto6').style.display="block";
-    document.getElementById('correcto6').style.display="none";
-    cb7.style.borderColor= 'red';
-    validacion[3] = 0;
+    validacion_incorrecta(6,cb);
     }
   else {
-    document.getElementById('texto-error6').style.display="none";
-    document.getElementById('incorrecto6').style.display="none";
-    document.getElementById('correcto6').style.display="block";
-    cb7.style.borderColor= 'green';
-    if (validacion[6] == 0) validacion[6] = 1;
+    validacion_correcta(6,cb);
   }
   return false;
 }
 function validar_nombre_local(nombre_local){
-  var cb8 = document.getElementById('colorborde7');
+  var cb = document.getElementById('colorborde7');
   var nombre_local=document.getElementById('nombre_local');
   if (nombre_local.value == "") {
-    document.getElementById('texto-error7').style.display="block";
-    document.getElementById('incorrecto7').style.display="block";
-    document.getElementById('correcto7').style.display="none";
-    cb8.style.borderColor= 'red';
-    validacion[7] = 0;
-    nombre_local.focus();
+    validacion_incorrecta(7,cb);
   }
   else if (nombre_local.value!="") 
     {
-    document.getElementById('correcto7').style.display="block";
-    document.getElementById('texto-error7').style.display="none";
-    document.getElementById('incorrecto7').style.display="none";
-    cb8.style.borderColor= 'green'; 
-    if (validacion[7] == 0) validacion[7] = 1; 
+    validacion_correcta(7,cb);
   }
   return false;
 }
@@ -203,11 +129,24 @@ function EnviarFormulario(){
   }
   return false;
 }
-
 function cambiarCosas(limite){
   for(let i = 0; i < limite; i++){
         const correcto = document.getElementById('correcto' + i).style.display="none";
         const color_borde = document.getElementById('colorborde' + i).style.borderColor='#aaa';
       }
   const activacion1=document.getElementById('activacion').style.display="none";
+}
+function validacion_correcta(n,cb){
+  document.getElementById('texto-error'+ n).style.display="none";
+  document.getElementById('incorrecto'+ n).style.display="none";
+  document.getElementById('correcto'+ n).style.display="inline-block";
+  cb.style.borderColor= 'green';
+  if (validacion[n] == 0) validacion[n] = 1;
+}
+function validacion_incorrecta(n,cb){
+  document.getElementById('texto-error'+ n).style.display="inline-block";
+  document.getElementById('incorrecto'+ n).style.display="inline-block";
+  document.getElementById('correcto'+ n).style.display="none";
+  cb.style.borderColor= 'red';
+  validacion[n] = 0;
 }
